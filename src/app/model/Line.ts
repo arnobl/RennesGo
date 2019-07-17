2e9540ee71ee1755266fe56a0f12b0d136448a5e
arnobl <1052622+arnobl@users.noreply.github.com>
2019-07-17 19:05:02 +0200

    move

--- src/app/model/Line.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Line {
-    id: string;
-    nomcourt: string;
-    nomLong: string;
-    couleurligne: string;
-    couleurtexteligne: string;
-    nomfamillecommerciale: string;
-}
--- src/app/model/Stop.ts
+++
@@ -1,6 +0,0 @@
-
-export class Stop {
-    constructor(private readonly name: string, private readonly direction: string) {
-
-    }
-}
