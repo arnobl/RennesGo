a7256af49cbddf90418a9c9306d1e11f8edeca65
Gauthier-Clerc François <francois@gauthier-clerc.fr>
2019-10-22 16:43:02 +0200

    Add new ComponentScan paths
    Add new ComponentScan for fixing jar link issue with spring boot maven plugin.

--- back-end/src/main/java/rennesgo/RennesGo.java
+++ back-end/src/main/java/rennesgo/RennesGo.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan({ "rennesgo.auth","rennesgo.controller", "rennesgo.data"})
 public class RennesGo {
 	public static void main(final String[] args) {
 		final SpringApplication app = new SpringApplication(RennesGo.class);
